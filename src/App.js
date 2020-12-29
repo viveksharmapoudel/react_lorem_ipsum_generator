386aa5c62733387f4f67c9f47d65056057b0c133
vivek <viveksharmapoudel@gmail.com>
2020-12-29 16:14:52 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import data from './data';
+
+
+function App(){
+
+  const [num, setNum]= useState(0);
+  const [paragraph, setParagraph]=useState([]);
+
+  const handleSubmit= (e) =>{
+    e.preventDefault();
+    (parseInt(num)<=0)?setParagraph(data.slice(0,1)):
+    (parseInt(num)>=data.length)?setParagraph(data):
+    setParagraph(data.slice(0,parseInt(num)))
+  }
+  return(
+    <section className="section-center">
+      <h3> Bored of normal Lorem ipsum?</h3>
+      <form className="lorem-form"
+        onSubmit={handleSubmit}
+      >
+        <label className="label">
+          Paragraphs:
+          <input name="num"
+                 type="number"
+                 value={num}
+                 onChange={(e) => {setNum(e.target.value)}}
+          />
+        </label>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <div className="result">
+        {
+          paragraph.map((singleP,index)=>{
+            return <p key={index}>{singleP}</p>
+          })
+        }
+      </div>
+    </section>
+  )
 }
 
 export default App;
